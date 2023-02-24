953b55c1a790a3d40352666848711cd23bea5a3c
joanbaldo <joanbaldo@hotmail.com>
2023-02-24 15:58:34 +0100

    FLAGS CH-3 WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
 console.log('Test conexión')
+// let kelvin = 283
+// let celsius = kelvin - 273.15
+// let fahrenheit = celsius - 186.52
+// console.log(`Tras convertir los ${kelvin}K, obtenemos ${celsius}C, y ${fahrenheit}F`)
+
