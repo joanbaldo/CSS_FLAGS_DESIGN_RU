540d667a1050610cb6c3065baf9696e5fa116752
joanbaldo <joanbaldo@hotmail.com>
2023-02-25 06:13:53 +0100

    FLAGS CM-3 Flag REf

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
 console.log('Test conexión')
-// let kelvin = 283
-// let celsius = kelvin - 273.15
-// let fahrenheit = celsius - 186.52
-// console.log(`Tras convertir los ${kelvin}K, obtenemos ${celsius}C, y ${fahrenheit}F`)
+
 
